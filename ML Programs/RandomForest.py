789bf9d45ed8ac7e1627bb78722b226654683333
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-08-27 17:10:40 +0530

    Update RandomForest.py

--- ML Programs/RandomForest.py
+++ ML Programs/RandomForest.py
@@ -1,6 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 X = [[30], [40], [50], [60], [20], [10], [70]]
